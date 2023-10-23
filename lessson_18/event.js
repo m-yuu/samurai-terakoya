f58814bb9ca3f7107c0d73a46768c4e0f1df3319
m-yuu <84389117+m-yuu@users.noreply.github.com>
2023-10-24 08:58:27 +0900

    lesson_18
    JSファイルのアップロード

---
+++ lessson_18/event.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById('output-btn');
+
+btn.addEventListener('click', () => {
+  console.log('クリックされました');
+});
+
+const addBtn = document.getElementById('add-btn');
+const parentList = document.getElementById('parent-list');
+
+addBtn.addEventListener('click', () => {
+  const childList = document.createElement('li');
+  childList.textContent = 'これはリスト要素です';
+
+  parentList.appendChild(childList);
+
+});
